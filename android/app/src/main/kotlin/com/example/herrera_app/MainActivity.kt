5e3cd03bd2d94622dd94c89c808490fde90030ad
isaacJ212 <isaacejimenez2006@gmail.com>
2026-09-07 15:17:21 -0600

    Creacion de la  estructura base del proyecto

---
+++ android/app/src/main/kotlin/com/example/herrera_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.herrera_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Screens/Reporte_de_Inventario/reporte_de_inventario_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ReporteDeInventarioScreen extends StatelessWidget {
+  const ReporteDeInventarioScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Reporte de Inventario Screen'),
+      ),
+    );
+  }
+}
