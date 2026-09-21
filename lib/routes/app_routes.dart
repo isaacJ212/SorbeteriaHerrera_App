0368219346cfd919a30ca759314c66df786a3dbe
isaacJ212 <isaacejimenez2006@gmail.com>
2026-09-21 13:46:21 -0600

    fix: falta de usuarios  en app_routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -6,6 +6,7 @@ abstract class AppRoutes {
   static const String reporteVenta = '/reporteVenta';
   static const String reporteInventario = '/reporteInventario';
   static const String notFound = '/not-found';
+  static const String usuarios = 'usuarios';
 
   static const String miAccount = '/myAccount';
   static const String userDetails = '/userDetail';
