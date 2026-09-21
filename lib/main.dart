571b2e1fb4670ba7d700b89a1610c9de0f6ad05f
isaacJ212 <isaacejimenez2006@gmail.com>
2026-09-20 22:51:02 -0600

    se maqueta la screen del historial de acceso al sistema

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sorbeteria Herrera',
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
-      initialRoute: AppRoutes.miAccount,
+      initialRoute: AppRoutes.Accesshistory,
       onGenerateRoute: AppRouter.onGenerateRoute,
       onUnknownRoute: AppRouter.onGenerateRoute,
       debugShowCheckedModeBanner: false,
